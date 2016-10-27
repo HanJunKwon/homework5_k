0452e6df517a865944c481d7434b9cdadcce6401
권한준 <skclaqks11@naver.com>
2016-10-27 23:43:47 +0900

    마르모 출력

---
+++ homework5_k/src/homework5_k/DiamondStars.java
@@ -0,0 +1,28 @@
+package homework5_k;
+
+import java.util.Scanner;
+
+public class DiamondStars {
+	public void print() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("다이아몬드의 크기입력 : ");
+		int hight = sc.nextInt();
+	
+		for (int i = 0; i < hight * 2 - 1; ++i) {
+			for (int j = 0; j < hight * 2 - 1; ++j) {
+				if (i < hight) {
+					if (j >= hight-i-1 && j<= hight+i-1) 
+							System.out.print("*");
+					else 
+						System.out.print(" ");
+				}
+				else {
+					if(j>=i-hight+1 && j<=hight*2-i+2)
+						System.out.print("*");
+					else System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
