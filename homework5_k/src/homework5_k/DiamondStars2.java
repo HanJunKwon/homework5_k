5df83223d5030cde2db353267a89fde79af536c1
권한준 <skclaqks11@naver.com>
2016-10-28 01:01:49 +0900

    다이아몬드 출력(나눠서)

---
+++ homework5_k/src/homework5_k/DiamondStars2.java
@@ -0,0 +1,30 @@
+package homework5_k;
+
+import java.util.Scanner;
+
+public class DiamondStars2 {
+	public void print() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("이등변삼각형 높이입력 : ");
+		int hight = sc.nextInt();
+
+		for (int i = 0; i < hight; ++i) {
+			for (int j = 0; j < hight * 2 - 1; ++j) {
+				if (j >= hight-i-1 && j<= hight+i-1) 
+						System.out.print("*");
+				else 
+					System.out.print(" ");
+			}
+			System.out.println();;
+		}
+		for(int i=0; i<hight-1; ++i){
+			for(int j=0; j<hight*2-1; ++j){
+				if(j>=i+1 && j<=hight*2-i-3)
+					System.out.print("*");
+				else 
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
